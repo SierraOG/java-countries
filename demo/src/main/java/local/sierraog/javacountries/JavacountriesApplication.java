676a65b9926879ca67de8cd996de85d7a719bd41
sierraog <sierrao@utexas.edu>
2020-05-28 20:05:05 -0500

    Added population controller

--- demo/src/main/java/local/sierraog/javacountries/JavacountriesApplication.java
+++ demo/src/main/java/local/sierraog/javacountries/JavacountriesApplication.java
@@ -1,12 +1,16 @@
 package local.sierraog.javacountries;
 
+import local.sierraog.javacountries.models.CountryList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavacountriesApplication {
 
-	public static void main(String[] args) {
+	public static CountryList ourCountryList;
+	public static void main(String[] args)
+	{
+		ourCountryList = new CountryList();
 		SpringApplication.run(JavacountriesApplication.class, args);
 	}
 
--- demo/src/main/java/local/sierraog/javacountries/controllers/PopulationController.java
+++ demo/src/main/java/local/sierraog/javacountries/controllers/PopulationController.java
@@ -1,4 +1,42 @@
 package local.sierraog.javacountries.controllers;
 
+import local.sierraog.javacountries.JavacountriesApplication;
+import local.sierraog.javacountries.models.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+import static local.sierraog.javacountries.JavacountriesApplication.ourCountryList;
+
+@RestController
+@RequestMapping("/population")
 public class PopulationController {
+    @GetMapping(value = "/size/{population}",
+            produces = {"application/json"})
+    public ResponseEntity<?> getCountriesByPopulationSize(@PathVariable long population)
+    {
+        ArrayList<Country> returnCountries = ourCountryList.filterCountries(c -> c.getPopulation() >= population);
+        return new ResponseEntity<>(returnCountries, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/min",
+            produces = {"application/json"})
+    public ResponseEntity<?> getCountryByMin()
+    {
+        ourCountryList.countryList.sort((c1, c2) -> c1.getPopulation() - c2.getPopulation());
+        return new ResponseEntity<>(ourCountryList.countryList.get(0), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/max",
+            produces = {"application/json"})
+    public ResponseEntity<?> getCountryByMax()
+    {
+        ourCountryList.countryList.sort((c1, c2) -> c2.getPopulation() - c1.getPopulation());
+        return new ResponseEntity<>(ourCountryList.countryList.get(0), HttpStatus.OK);
+    }
 }
